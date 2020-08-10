48f2fcb6f9986ba23495e5b0e7342ef5ce0714ff
Christian Shaw <cshaw00048@gmail.com>
2020-08-10 09:17:31 -0500

    javascript continued

--- giphy.js
+++ giphy.js
@@ -27,6 +27,29 @@ $(document).on("click", ".band-button", function() {
   var type = $(this).attr("data-type");
   var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response){
+      var results = response.data;
+
+      for (var i = 0; i = results.length; i++) {
+        var animalDiv = $("<div class=\"band-item\">");
+
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var animated = results [i]. rating; 
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var animated = results [i].images.fixed_height.url;
+        var still = results[i].images.fixed_height_still.url;
+      }
+    })
+
 })
 
 })
